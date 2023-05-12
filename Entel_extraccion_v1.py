05f4e147f7a82a03c64d95b032ba02729930275d
Julio9377 <juliocesar9377@hotmail.com>
2023-05-12 11:17:49 -0500

    ok

--- Entel_extraccion_v1.py
+++ Entel_extraccion_v1.py
@@ -20,8 +20,24 @@ import requests
 import re
 from bs4 import BeautifulSoup
 
-
-
+url = "https://www.entel.pe/empresas/catalogo/equipos/"
+response = requests.get(url)
+soup = BeautifulSoup(response.text, 'html.parser')
+divs = soup.find_all('div')
+grupo_div = soup.find('div',class_='e-catalog-equipment__grid__list e-box-inline-block-ts e-box-inline-block--clear-tm')
+lista = []
+try:
+    for div in grupo_div.find_all('div', recursive=False):
+        href = div.div.a['href']
+        if href=='javascript:void(0);':
+            href = div.div['href']
+            lista.append(href)
+        else:
+            href = div.div['href']
+            lista.append(href)
+except AttributeError:
+    pass
+print(len(lista))
 
 options =  webdriver.ChromeOptions()
 options.add_argument('--start-maximized')
@@ -33,21 +49,10 @@ driver = webdriver.Chrome(r"D:\ChromeDriver\chromedriver.exe",chrome_options=opt
 archivo_exportado = "D:\\Marcos_Entel\\Datos_Entel.csv"
 archivo_backup = "D:\\Marcos_Entel\\Datos_Entel_backup.csv"
 # Inicializamos el navegador
-
-Lista = ['oppo-reno-7-oppo-enco-air-2','iphone-14-pro-128gb-cargador-apple-20w','xiaomi-redmi-note-12-buds-essential','oppo-a77-neckband','oppo-reno-6-lite-neckband','honor-magic-5-lite-bindu-audifonos','iphone-14-plus-512gb','samsung-galaxy-s21-fe-128gb','iphone-14-128gb','vivo-v21','iphone-13-pro-256gb','iphone-14-512gb','iphone-14-plus-256gb','iphone-14-pro-256gb','iphone-14-pro-max-256gb','iphone-14-pro-max-512gb','motorola-moto-e22i','honor-70-256gb','oppo-reno-7','motorola-moto-g23','samsung-galaxy-z-flip-4-128gb','samsung-galaxy-z-flip-4-256gb','oppo-a77','xiaomi-redmi-a2','samsung-galaxy-s23-128gb','samsung-galaxy-a23','vivo-y55','honor-magic-5-lite','oppo-reno-6-lite','honor-x9','samsung-galaxy-s23-256gb','samsung-galaxy-s23-plus-512gb','samsung-galaxy-s23-ultra-512gb','zte-a51-lite','samsung-galaxy-s23-plus-256gb','samsung-galaxy-s23-ultra-256gb','iphone-13-128gb','edge-30-fusion-audifonos','edge-30-neo','edge-30-neo-audifonos','galaxy-a04e','iphone-14-plus-128gb','iphone-14-pro-128gb','iphone-14-pro-max-128gb','zte-blade-vita-v40','apple-iphone-11-64gb','honor-70-256gb-earbuds-x','samsung-galaxy-s20-fe-5g','motorola-moto-g22','honor-x8a','motorola-moto-g53','vivo-y22s','samsung-galaxy-a33','xiaomi-redmi-10c-364gb','samsung-galaxy-a04','redmi-note-11s-128gb-5g','oppo-a57-128gb','honor-x6','oppo-a17','honor-x7a','samsung-galaxy-a14','xiaomi-redmi-note-12','redmi-note-11-128gb-buds-essential','xiaomi-redmi-12c','zte-blade-a53-plus','xiaomi-redmi-a1','samsung-galaxy-a53','motorola-moto-g31','zte-blade-v30-vita','xiaomi-redmi-note-10-pro','oppo-a54','samsung-galaxy-a03-core','iphone-13-pro-max-128gb','iphone-13-pro-128gb','iphone-14-256gb','xiaomi-redmi-10-2022','iphone-14-pro-512gb','iphone-13-512gb','iphone-13-pro-512gb','samsung-galaxy-z-fold-4-256gb','samsung-galaxy-z-fold-4-512gb','redmi-10c','redmi-note-11s','honor-x7','samsung-galaxy-s22','honor-50-256gb','huawei-nova-9-se','zte-blade-a5-2020-32gb','honor-70-128gb','samsung-galaxy-s22-plus','samsung-galaxy-s22-ultra','samsung-galaxy-a52s','honor-x8','huawei-nova-y70','xiaomi-redmi-10a','xiaomi-12','motorola-moto-g50','iphone-13-256gb','xiaomi-redmi-note-11-128gb','iphone-13-mini-256gb','apple-iphone-se-2022-64gb','iphone-se-2022-256gb','iphone-13-pro-max-256gb','samsung-galaxy-a22','galaxy-a13','iphone-13-mini-128gb','honor-x8-pack','huawei-nova-9-band','samsung-galaxy-a03s','xiaomi-redmi-9c','motorola-moto-edge-30-pro-5g','iphone-14-cargador','iphone-14-plus-airpods','xiaomi-redmi-note-11-pro-128gb','apple-iphone-12-128gb','xiaomi-redmi-10-5g','motorola-moto-e32','motorola-moto-edge-20-lite','samsung-galaxy-a22-64','iphone-se-2022-128gb']
-
-
-url_base = 'https://www.entel.pe/empresas/catalogo/equipo/'
-url_params = '/?modalidad=migra&forma_pago=contado'
-urls=[]
 datos = []
-for item in Lista:
-    url = url_base + item + url_params
-    urls.append(url)
-print(item)
-
-for url in urls:
-    driver.get(url) # Entramos URL x URL
+for url_link in lista:
+    print(url_link)
+    driver.get(url_link) # Entramos URL x URL
 
     try:
         accept_cookies_button = driver.find_element(By.XPATH,"//button[text()='Aceptar']")
@@ -82,7 +87,7 @@ for url in urls:
                     except StaleElementReferenceException:
                         continue
             Resumen = driver.find_element("xpath",'/html/body/div[4]/div[1]/section[1]/div/div[3]/div[2]/div[2]/div[4]/div/div[1]').text
-            print(Resumen)
+            #print(Resumen)
             Resumen2 = Resumen.replace("\n"," ").replace("\s+"," ").replace("Resumen de pedido ","")\
                             .replace(" Equipo: Cantidad de equipos 1 Precio total del equipo ","|CONTADO|")\
                             .replace(" Equipo: Cantidad de equipos 1 CUOTA INICIAL: ","|CUOTA|").replace("S/ ","").replace(" Plan: Empresa Pro ","|").replace(" Chip y delivery: Gratis","")
